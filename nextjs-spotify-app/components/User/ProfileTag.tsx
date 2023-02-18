e5abb2d09ad47ecd2fd4c63b7492a05ffe40159d
Dawaad <jntucker02@gmail.com>
2023-02-18 16:40:43 +1100

    updated login screen

--- nextjs-spotify-app/components/User/ProfileTag.tsx
+++ nextjs-spotify-app/components/User/ProfileTag.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import { useSession } from 'next-auth/react'
-import { ChevronDownIcon } from '@heroicons/react/24/solid'
+import React from "react";
+import { signOut, useSession } from "next-auth/react";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 function ProfileTag() {
-    const {data:session} = useSession()
+  const { data: session } = useSession();
   return (
-    <header className="absolute top-5 right-8 z-10">
-    <div className="flex items-center bg-zinc-900 space-x-3 bg-opacity-90 hover:bg-opacity-80 rounded-full cursor-pointer text-white p-1 pr-2">
-      
-      <img
-        className="rounded-full w-10 h-10"
-        src={session?.user?.image as string}
-        alt="Profile Image"
-      />
-      <h2 className='hidden md:block'>{session?.user?.name}</h2>
-      <ChevronDownIcon className="w-5 h-5" />
-    </div>
-  </header>
-  )
+    <header className="absolute top-5 right-8 z-10 space-y-3">
+      <div className="flex items-center bg-zinc-900 space-x-3 bg-opacity-90 hover:bg-opacity-80 rounded-full cursor-pointer text-white p-1 pr-2">
+        <img
+          className="rounded-full w-10 h-10"
+          src={session?.user?.image as string}
+          alt="Profile Image"
+        />
+        <h2 className="hidden md:block">{session?.user?.name}</h2>
+        <ChevronDownIcon className="w-5 h-5" />
+      </div>
+      <div className="text-zinc-200 bg-zinc-900 bg-opacity-90 rounded-lg p-1 space-x-2 list-none">
+        
+        <li
+         onClick={() => {
+          signOut()}}
+         className=" grid grid-cols-2 p-2 hover:bg-zinc-700 hover:bg-opacity-80 rounded-lg">
+          <p>Logout</p>
+          <div className="flex items-center justify-end">
+            <ArrowRightOnRectangleIcon className="w-5 h-5 mr-3" />
+          </div>
+        </li>
+      </div>
+    </header>
+  );
 }
 
-export default ProfileTag
+export default ProfileTag;
--- nextjs-spotify-app/pages/login.tsx
+++ nextjs-spotify-app/pages/login.tsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { getProviders, signIn } from "next-auth/react";
 import { GetServerSideProps, NextPage } from "next";
 
 const login: NextPage<{ providers: any[] }> = ({ providers }) => {
+  if (typeof window !== "undefined") {
+    const mouseTracker = document.getElementById("mouseTracker");
+
+    document.body.onpointermove = (event) => {
+      const { clientX, clientY } = event;
+      (mouseTracker as HTMLElement).animate(
+        {
+          left: `${clientX}px`,
+          top: `${clientY}px`,
+        },
+        { duration: 3000, fill: "forwards" }
+      );
+    };
+  }
+
   return (
-    <div className="flex flex-col items-center bg-zinc-800 min-h-screen w-full justify-center">
+    <div className="flex flex-col items-center bg-zinc-800 h-screen m-0 justify-center overflow-hidden">
+      <div id="mouseTracker"></div>
+      <div id="blur"></div>
+      <div className="z-[3]">
+
       <img className="w-52 mb-5" src="https://links.papareact.com/9xl" />
 
       {Object.values(providers).map((provider) => (
         <div key={provider}>
-          <button className="bg-[#18D860] text-white p-5 rounded-lg"
-          onClick={() => signIn(provider.id, {callbackUrl: "/"})}>
-
-          Login with {provider.name}
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-lg"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+            Login with {provider.name}
           </button>
-          </div>
+        </div>
       ))}
+      </div>
     </div>
   );
 };
